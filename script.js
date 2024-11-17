b276eaa0b2d944a3e655df31b5bc5edb801141e4
Lucie <lucieletorey1995@hotmail.fr>
2024-11-17 08:35:43 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Exemple d'ajout au panier (fonctionnalité simulée)
+document.querySelectorAll('#ajouter-panier').forEach(button => {
+    button.addEventListener('click', function() {
+      alert('Produit ajouté au panier');
+    });
+  });
+  
